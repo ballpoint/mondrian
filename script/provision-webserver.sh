d4503a8c35bdef9ad3d8c4523f8924394a7a8935
Artur Sapek <me@artur.co>
2017-10-29 17:08:51 -0400

    more setup

--- script/provision-webserver.sh
+++ script/provision-webserver.sh
@@ -1,2 +1,5 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+mkdir -p /app/mondrian/{releases,shared}
+mkdir -p /app/mondrian/shared/node_modules
